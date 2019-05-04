ece5d725c6e13b21ee31c1282beb518200e39f56
joelba93 <jo.ba@bluewin.ch>
2019-05-04 19:13:28 +0200

    minimize sensorsize function

--- sample/minimize_sensorsize.py
+++ sample/minimize_sensorsize.py
@@ -1,24 +1,32 @@
 import os
 import pandas as pd
-from Tracking_Particle_Filter.tracking import load_events
+from helpers import load_events
 
 # Todo: Finish
 size=64
 
 events, num_events = load_events('events.txt',head=None , return_number=True)
 
-events_small_sensor = pd.DataFrame(columns=['sec', 'nsec', 'x', 'y', 'pol'])
+events_small_sensor = pd.DataFrame(columns=['t', 'x', 'y', 'pol'])
 
+'''
 def check_and_write(row):
-    if row['x'] <= size:
-        events_small_sensor.append(row)
-        print('okok')
-    return
+    if row['x'].values <= size:
+        df = events_small_sensor.append(row)
+        return
 
-check_and_write(events.iloc[2])
+print(check_and_write(events.iloc[[2]]))
 
-events_small_sensor.append(df2)
+events.iloc[[1]].apply(lambda r: check_and_write(r))
+'''
+
+for i in range(3624659):
+    print(i)
+    if events.iloc[[i]]['x'].values <= size and events.iloc[[i]]['y'].values <= size:
+        events_small_sensor = events_small_sensor.append(events.iloc[[i]])
+    else:
+        None
+
 print(events_small_sensor)
 
-
-# events.apply(lambda row check_and_write(row))
+events_small_sensor.to_csv('/Users/JoelBachmann/Desktop/FS19/3D Vision/Project/Event-Based-Camera-Simultaneous-Mosaicing-and-Tracking/Tracking_Particle_Filter/events_small_sensor.txt', header=None, index=None, sep=' ', mode='a')
