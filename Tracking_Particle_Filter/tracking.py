Found bug! Was a problem of visualization, multiplication with vector orthogonal to x-rotation [1, 0, 0])

Tracking_Particle_Filter/tracking.py
@@ -36,18 +36,20 @@ outputdir_poses = '../output/poses/'
 
 # Constants
 eventlikelihood_comparison_flipped = True
-num_particles = 2
+num_particles = 1
 num_events_batch = 300
-sigma_init1 = 0.1
-sigma_init2 = 0.1
-sigma_init3 = 0.1
-factor = 0
+sigma_init1 = 0
+sigma_init2 = 0
+sigma_init3 = 0
+factor = 1
 sigma_likelihood = 8.0*1e-2
 sigma_1 = factor * 3.3663987633184266e-05# sigma1 for motion update
 sigma_2 = factor * 3.366410184326084e-05# sigma2 for motion update
 sigma_3 = factor * 0.0005285784750737629 # sigma3 for motion update
 total_nr_events_considered = 130100  #TODO: Only works if not dividable by events by batch
 first_matrix = helpers.get_first_matrix(filename_poses)
+
+all_rotations_test = []
 
 
 # tau=7000
@@ -308,8 +310,19 @@ def motion_update(particles, tau):
     G2 = np.array([[0, 0, 1], [0, 0, 0], [-1, 0, 0]])  # rotation around y
     G3 = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 0]])  # rotation around z
 
-    R_c = sp.expm(np.dot(10004**4*0.0005285784750737629**2, G1))
+    # R_c = sp.expm(np.dot(10004**4*0.0005285784750737629**2, G3))
+    m = 10004**4
+    R_c = sp.expm(np.dot(m*sigma_1**2, G1) +
+                  np.dot(m*sigma_2**2, G2) +
+                  np.dot(m*sigma_3**2, G3))
+
+
+    R_c = sp.expm(
+                  np.dot(m*sigma_2**2, G1))
+
     # print(R_c)
+    # print(particles['Rotation'].loc[0])
+    # exit()
 
     particles['Rotation'] = particles['Rotation'].apply(
         # lambda x: np.dot(x, np.dot(R_c, sp.expm(np.dot(np.random.normal(0.0, sigma_1**2 * tau * num_events_batch), G1) +
@@ -318,7 +331,6 @@ def motion_update(particles, tau):
 
         lambda x: np.dot(x, R_c))
 
-    # print(particles['Rotation'].loc[0])
     return particles
 
 
@@ -448,7 +460,7 @@ def mean_of_resampled_particles(particles):
     :param particles: pandas df of resampled particles (all with the same weight)
     :return: mean of rotation matrix
     '''
-    rotmats=np.zeros((len(particles),3,3))
+    rotmats=np.zeros((len(particles), 3, 3))
     for i in range(len(particles)):
         rotmats[i] = sp.logm(particles['Rotation'].as_matrix()[i])
     liemean = sum(rotmats)/len(particles)
@@ -495,6 +507,7 @@ def run():
     mean_of_rotations = pd.DataFrame(columns=['Rotation'])
     mean_of_rotations['Rotation'].astype(object)
 
+    all_rotations_test.append(first_matrix)
     while batch_nr < num_batches:
         events_batch = events[event_nr:event_nr + num_events_batch]
         dt_batch = (events_batch['t'].max() - events_batch['t'].min())/num_events_batch
@@ -514,6 +527,8 @@ def run():
         t_batch = events.loc[event_nr]['t']
 
         new_rotation = mean_of_resampled_particles(particles)
+        all_rotations_test.append(new_rotation)
+
 
         # visualize_particles(particles['Rotation'],  mean_value=new_rotation)
 
@@ -528,8 +543,9 @@ def run():
 
     print(batch_nr)
     print(event_nr)
-    datestring = helpers.write_quaternions2file(all_rotations,
-                                                directory='../output/poses/')
+    quaternions = helpers.rot2quaternions(all_rotations)
+    datestring = helpers.quaternions2file(quaternions, directory='../output/poses/')
+
     #Include all wished
     time_passed = round(time.time() - starttime)
     helpers.write_logfile(datestring, directory= '../output/poses/',
@@ -551,6 +567,7 @@ def run():
     print("Time passed: {} sec".format(time_passed))
     # visualisation.visualize_particles(mean_of_rotations['Rotation'], mean_value = None)
     print("Done")
+    return all_rotations
 
 
 if __name__ == '__main__':

sample/visualisation.py
@@ -27,7 +27,9 @@ def compare_trajectories(df_ours, df_theirs):
     """
 
 
-    vec = np.array([1,0,0]).T
+    # vec = np.array([1,0,0]).T
+    vec = np.array([np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3)]).T
+
     vecM = df_ours['Rotation'].apply(lambda x: np.dot(x, vec))
     rotX = vecM.str.get(0)
     rotY = vecM.str.get(1)
@@ -61,6 +63,35 @@ def cut_df_wrt_time(rotations_ours, rotations_theirs):
 
     return rotations_theirs_cut
 
+def visualize_rotmats(rotation_matrices):
+    vec = np.array([np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3)]).T
+    # vec = np.array([0, 0, 1]).T
+
+    rotX = []
+    rotY = []
+    rotZ = []
+    for rotmat in rotation_matrices:
+        vecM = np.dot(vec, rotmat)
+        rotX.append(vecM[0])
+        rotY.append(vecM[1])
+        rotZ.append(vecM[2])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim3d(-1, 1)
+    ax.set_ylim3d(-1, 1)
+    ax.set_zlim3d(-1, 1)
+    p = ax.scatter(rotX, rotY, rotZ, c='b')
+    p = ax.scatter(rotX[0], rotY[0], rotZ[0], c='r', s=200)
+
+    # cbar = fig.colorbar(p, ax=ax)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    # cbar.set_label("Nr. of pose")
+    # return ax
+    plt.show()
+
 
 def visualize_particles(rotation_matrices, mean_value = None):
     """
@@ -72,10 +103,11 @@ def visualize_particles(rotation_matrices, mean_value = None):
     """
 
     # vec = np.array([1,0,0]).T
-    vec = np.array([np.sqrt(1/3), np.sqrt(1/3), np.sqrt(1/3)]).T
-    vec = np.array([0, 0, 1]).T
+    vec = np.array([np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3)]).T
+    # vec = np.array([0, 0, 1]).T
 
-
+    print(rotation_matrices)
+    exit()
     vecM = rotation_matrices.apply(lambda x: np.dot(x, vec))
     rotX = vecM.str.get(0)
     rotY = vecM.str.get(1)
@@ -119,7 +151,7 @@ def plot_unitsphere_matplot():
 
 if __name__ == '__main__':
     directory_poses = '../output/poses/'
-    filename_ours = 'quaternions_06052019T132925.txt'
+    filename_ours = 'quaternions_07052019T150842.txt'
     filename_theirs = 'poses.txt'
     poses_ours = helpers.load_poses(filename_poses=os.path.join(directory_poses, filename_ours))
     poses_theirs = helpers.load_poses(filename_poses=os.path.join(directory_poses, filename_theirs),
@@ -131,4 +163,4 @@ if __name__ == '__main__':
     rotations_theirs = coordinate_transforms.q2R_df(poses_theirs)
     rotations_theirs_cut = cut_df_wrt_time(rotations_ours, rotations_theirs)
 
-    compare_trajectories(rotations_ours,rotations_theirs_cut)
+    compare_trajectories(rotations_ours,rotations_theirs)