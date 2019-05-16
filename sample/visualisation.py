5a29ccdb6b28a6e97a047b701efd0b8b3c8c289e
huetufemchopf <celine.nauer@gmail.com>
2019-05-16 19:34:45 +0200

    Merge branch 'master' of github.com:nkdnnlr/Event-Based-Camera-Simultaneous-Mosaicing-and-Tracking
    # Conflicts:
    #	Tracking_Particle_Filter/tracking.py
    #	sample/visualisation.py

--- sample/visualisation.py
+++ sample/visualisation.py
@@ -16,6 +16,37 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import sample.coordinate_transforms as coordinate_transforms
 import sample.helpers as helpers
+import Tracking_Particle_Filter.tracking as track
+
+
+def compare_trajectories_2d(intensity_map, poses, event0):
+    '''
+
+    :param intensity_map:
+    :param poses:
+    :param event0:
+    :return: plot with intensity map and ground truth trajectory
+    '''
+    poses_converted = pd.DataFrame(columns=
+                          ['Rotation', 'Weight', 'theta',
+                           'phi', 'v', 'u', 'pol',
+                           'p_w1', 'p_w2', 'p_w3',
+                           'z', 'logintensity_ttc',
+                           'logintensity_t'])
+    poses_converted['Rotation'] = poses['Rotation']
+    poses_converted['Rotation'].astype('object')
+    tracker = track.Tracker()
+    calibration = tracker.camera_intrinsics()
+    calibration_inv = np.linalg.inv(calibration)
+    print(poses_converted['phi'])
+    for idx, event in event0.iterrows():
+        angles = tracker.event_and_particles_to_angles(event, poses_converted, calibration_inv)
+        continue
+
+    plt.figure()
+    plt.imshow(intensity_map)
+    plt.show()
+
 
 
 
@@ -164,15 +195,22 @@ def plot_unitsphere_matplot():
 
 
 if __name__ == '__main__':
-    directory_poses = '../output/poses/'
 
-    #ground truth
+    directory_poses = '../output/poses/'
+    data_dir = '../data/synth1'
+    intensity_map = np.load('../output/intensity_map.npy')
+    event_file = os.path.join(data_dir, 'events.txt')
+    event0 = helpers.load_events(filename=event_file, head=1)
+
+    # ground truth
     filename_groundtruth = 'poses.txt'
     poses_groundtruth = helpers.load_poses(filename_poses=os.path.join(directory_poses, filename_groundtruth),
                                            includes_translations=True)
     rotations_groundtruth = coordinate_transforms.q2R_df(poses_groundtruth)
-    # rotations_groundtruth_cut = cut_df_wrt_time(rotations_ours, rotations_groundtruth)
+    compare_trajectories_2d(intensity_map, rotations_groundtruth, event0)
 
+    # rotations_groundtruth_cut = cut_df_wrt_time(rotations_ours, rotations_groundtruth)
+'''
     filename_onlymotionupdate = 'quaternions_11052019T150554_onlymotionupdate.txt'
     filename_likelihoodFalse = 'quaternions_13052019T113443_20deg_False.txt'
     filename_likelihoodTrue = 'quaternions_14052019T091627_50deg_True_1000particles.txt'
@@ -194,3 +232,4 @@ if __name__ == '__main__':
                          likelihoodFlippedFalse=rotations_likelihoodFalse,
                          likelihoodFlippedTrue=rotations_likelihoodTrue,
                          likelihoodFlippedTruessmall=rotations_likelihoodTruessmall)
+'''
